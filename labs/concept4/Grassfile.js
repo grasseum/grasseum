843f824c1ff3a17731daa967f7b02daa89704d66
Pein Freecs <plants.coordinators@gmail.com>
2020-06-05 22:21:08 +0800

    in labs directory, adding 'dest' as output folder

--- labs/concept4/Grassfile.js
+++ labs/concept4/Grassfile.js
@@ -1,38 +1,26 @@
 
 ///module.exports=function(grassconf){ 
+
+  var grass_composer = require("grass_composer")
  exports.module=function(grassconf){   
 
-  
-  
+   
 
-  grassconf.load("test1",function(test){
-        console.log(test.get_content_from_pre_load(),"test1");
-        test.setDirectory({
-          "srcDir":[
-        
-            __dirname+"/src1/",
-          ],
-          "destDir":__dirname+"/dest/"
-        });
-        var local_filename = __dirname+"/";
-       // test.pipe("grass_minify",local_filename+"test.min.js",2)
-      // test.pipe("grass_composer",{
-      //   "data":{"ggg":"&&&3&&&"}
-      // })
-        test.pipe("grass_web_assets",'http://codehyouka.xyz/pfexp/frame/pf-ui/css/pf-ui-themes-one.css',local_filename+"test2.min.js",2)
-        //grss_concat
+
+ 
+
+  grassconf.load("test2",function(test){
+    return grassconf.download(["https://nodejs.org/api/stream.html","https://en.wikipedia.org/wiki/Gundam","http://codehyouka.xyz/pfexp/frame/pf-ui/css/pf-ui-themes-one.css"])
+       
+      .pipe(grassconf.dest("dest" ))
     })
 
-    grassconf.load("test23",function(test){
-
-
-
-    });
+   
     } 
 
-exports.execute=function(){   
+exports.execute=function(lib){   
 
 
-  return ['test1','test23']
+  return lib.series("test2")
 }     
   
